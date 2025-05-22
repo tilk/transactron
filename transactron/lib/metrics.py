343500bb1971de64b0864198478420c4b68e5e8e
Marek Materzok <tilk@tilk.eu>
2025-05-22 20:14:54 +0200

    Fix disabled metrics (#71)

--- transactron/lib/metrics.py
+++ transactron/lib/metrics.py
@@ -11,7 +11,6 @@ from amaranth.utils import bits_for, ceil_log2, exact_log2
 from transactron.utils import OneHotSwitchDynamic, ValueBundle
 from transactron import Method, Methods, def_methods, TModule
 from transactron.lib import FIFO, AsyncMemoryBank, logging
-from transactron.utils._typing import MethodStruct
 from transactron.utils.amaranth_ext.functions import and_value, max_value, min_value, or_value, sum_value, popcount
 from transactron.utils.dependencies import ListKey, DependencyContext, SimpleKey
 
@@ -124,9 +123,19 @@ class HwMetricsEnabledKey(SimpleKey[bool]):
     none of theirs signals will be synthesized.
     """
 
-    lock_on_get = False
     empty_valid = True
     default_value = False
+
+
+# TODO: find a cleaner way to make metric methods disappear when disabled.
+class DummyMethod(Method):
+    """
+    Hacky way to make a method ignore calls, allowing it not to be defined and
+    making it not appear in the final call graph.
+    """
+
+    def __call__(self, *args, **kwargs):
+        return Signal(StructLayout({}))
 
 
 class HwMetric(ABC, MetricModel):
@@ -186,14 +195,11 @@ class HwMetric(ABC, MetricModel):
     def wrap_method(method: _T_Method) -> _T_Method:
         if not HwMetric.metrics_enabled():
 
-            def dummy(*args, **kwargs) -> MethodStruct:
-                return Signal(StructLayout({}))
-
             if isinstance(method, Method):
-                method.__call__ = dummy
+                method.__class__ = DummyMethod
             else:
                 for m in method:
-                    m.__call__ = dummy
+                    m.__class__ = DummyMethod
         return method
 
     # To restore hashability lost by dataclass subclassing
