c1bf616467f0ce4b79a379eefa9cae26dfa5f837
Marek Materzok <tilk@tilk.eu>
2025-06-24 19:26:28 +0200

    Documentation and naming

--- transactron/lib/__init__.py
+++ transactron/lib/__init__.py
@@ -1,7 +1,7 @@
 from .allocators import *  # noqa: F401
 from .fifo import *  # noqa: F401
 from .connectors import *  # noqa: F401
-from .buttons import *  # noqa: F401
+from .basicio import *  # noqa: F401
 from .adapters import *  # noqa: F401
 from .transformers import *  # noqa: F401
 from .reqres import *  # noqa: F401
