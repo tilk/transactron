bdb9fa965cedf2236a8f33c4a013514588627152
Marek Materzok <tilk@tilk.eu>
2025-07-21 21:48:12 +0200

    Remove module argument from Method.proxy (#78)

--- transactron/core/method.py
+++ transactron/core/method.py
@@ -137,18 +137,14 @@ class Method(TransactionBase["Transaction | Method"]):
             return self._body_ptr
         raise RuntimeError(f"Method '{self.name}' not defined")
 
-    def _set_impl(self, m: TModule, value: "Body | Method"):
+    def _set_impl(self, value: "Body | Method"):
         if self._body_ptr is not None:
             raise RuntimeError(f"Method '{self.name}' already defined")
         if value.data_in.shape() != self.layout_in or value.data_out.shape() != self.layout_out:
             raise ValueError(f"Method {value.name} has different interface than {self.name}")
         self._body_ptr = value
-        m.d.comb += self.ready.eq(value.ready)
-        m.d.comb += self.run.eq(value.run)
-        m.d.comb += self.data_in.eq(value.data_in)
-        m.d.comb += self.data_out.eq(value.data_out)
 
-    def proxy(self, m: TModule, method: "Method"):
+    def proxy(self, method: "Method"):
         """Define as a proxy for another method.
 
         The calls to this method will be forwarded to `method`.
@@ -161,7 +157,10 @@ class Method(TransactionBase["Transaction | Method"]):
         method : Method
             Method for which this method is a proxy for.
         """
-        self._set_impl(m, method)
+        self._set_impl(method)
+
+        manager = DependencyContext.get().get_dependency(TransactionManagerKey())
+        manager._add_proxy_method(self)
 
     @contextmanager
     def body(
@@ -231,7 +230,7 @@ class Method(TransactionBase["Transaction | Method"]):
         body = Body(
             name=self.name, owner=self.owner, i=self.layout_in, o=self.layout_out, src_loc=self.src_loc, **kwargs
         )
-        self._set_impl(m, body)
+        self._set_impl(body)
 
         m.d.av_comb += body.ready.eq(ready)
         m.d.top_comb += body.data_out.eq(out)
