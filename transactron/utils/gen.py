184adb7d91b33f39da3c0765408034938e3ee132
Janeczko Jakub <kuba@ev1.pl>
2026-08-27 13:47:33 +0200

    remove undriven signal detection (#239)

--- transactron/utils/gen.py
+++ transactron/utils/gen.py
@@ -5,7 +5,6 @@ from typing import Iterable, Optional, TypeAlias
 from amaranth import *
 from amaranth.back import verilog
 from amaranth.hdl import Fragment, ValueCastable
-from amaranth.hdl._ast import SignalSet
 from amaranth.build import Platform
 from amaranth_types import AbstractInterface
 
@@ -241,23 +240,12 @@ class VerilogDebugWrapper(Elaboratable):
         elaboratable = Fragment.get(self.elaboratable, platform)
         m.submodules.elaboratable = elaboratable
 
-        # find all driven sigs in order to identify undriven ones
-        # undriven signals don't appear in synthesized files
-        driven_sigs = SignalSet()
-
-        def collect_driven_sigs(frag: Fragment):
-            for subfrag in frag.subfragments:  # type: ignore
-                collect_driven_sigs(subfrag[0])
-            for stmt in frag.statements.values():  # type: ignore
-                driven_sigs.update(stmt._lhs_signals())  # type: ignore
-
-        collect_driven_sigs(elaboratable)
+        v = Signal(0)
 
         def to_signal(val: Value | ValueCastable) -> Signal:
             val = Value.cast(val)
             if isinstance(val, Signal):
-                if val not in driven_sigs:
-                    m.d.comb += val.eq(val.init)
+                m.d.comb += v.eq(val[:0])  # force generation of `val` in verilog
                 return val
             else:
                 sig = Signal.like(val)
